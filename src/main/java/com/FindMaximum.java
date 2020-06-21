74364f6b4405f3d13c904548f0f630c42ac97447
Seetharamulu T <thumurugoti.seetharamulu@gmail.com>
2020-06-21 19:16:32 +0530

    Refactored Generic method to find maximum

--- src/main/java/com/FindMaximum.java
+++ src/main/java/com/FindMaximum.java
@@ -1,34 +1,16 @@
 package com;
 
+import java.util.Arrays;
+
 public class FindMaximum {
 
-    public static int integerMaximum(int a,int b, int c){
-        int max = a;
-        if(b>max){
-            max = b;
+    public static <E extends Comparable<E>> E maximum(E inputOne,E inputTwo,E inputThree){
+        E max = inputOne;
+        if (inputTwo.compareTo(max)>0){
+            max = inputTwo;
         }
-        if(c>max){
-            max = c;
-        }
-        return max;
-    }
-    public static float floatMaximum(float a,float b, float c){
-        float max = a;
-        if(b>max){
-            max = b;
-        }
-        if(c>max){
-            max = c;
-        }
-        return max;
-    }
-    public static String stringMaximum(String a,String b,String c){
-        String max = a;
-        if(b.compareTo(max) > 0){
-            max = b;
-        }
-        if(c.compareTo(max) > 0){
-            max = c;
+        if (inputThree.compareTo(max)>0){
+            max = inputThree;
         }
         return max;
     }
--- src/test/java/com/MaximumTest.java
+++ src/test/java/com/MaximumTest.java
@@ -5,48 +5,19 @@ import org.junit.Test;
 
 public class MaximumTest {
     @Test
-    public void maximumInteger_atPositionOne_shouldBeTrue() {
-    int actualValue = FindMaximum.integerMaximum(3,2,1);
+    public void maximumInteger() {
+    int actualValue = FindMaximum.maximum(1,2,3);
         Assert.assertEquals(3,actualValue);
     }
     @Test
-    public void maximumInteger_atPositionTwo_shouldBeTrue() {
-        int actualValue = FindMaximum.integerMaximum(2,3,1);
-        Assert.assertEquals(3,actualValue);
+    public void maximumDouble() {
+        float actualValue = FindMaximum.maximum(1.2f,2.2f,1.3f);
+        Assert.assertEquals(2.2f,actualValue,0.0f);
     }
     @Test
-    public void maximumInteger_atPositionThree_shouldBeTrue() {
-        int actualValue = FindMaximum.integerMaximum(1,2,3);
-        Assert.assertEquals(3,actualValue);
+    public void maximumString() {
+        String  actualValue = FindMaximum.maximum("apple","peach","banana");
+        Assert.assertEquals("peach", actualValue);
     }
-    @Test
-    public void maximumFloat_atPositionOne_shouldBeTrue() {
-        float actualValue = FindMaximum.floatMaximum(3.3f,3.2f, 3.1f);
-        Assert.assertEquals(3.3f,actualValue,0.0f);
-    }
-    @Test
-    public void maximumFloat_atPositionTwo_shouldBeTrue() {
-        float actualValue = FindMaximum.floatMaximum(2.3f,3.2f, 1.1f);
-        Assert.assertEquals(3.2f,actualValue,0.0f);
-    }
-    @Test
-    public void maximumFloat_atPositionThree_shouldBeTrue() {
-        float actualValue = FindMaximum.floatMaximum(1.3f,2.2f, 3.1f);
-        Assert.assertEquals(3.1f,actualValue,0.0f);
-    }
-    @Test
-    public void maximumString_atPositionOne_shouldBeTrue() {
-        String  actualValue = FindMaximum.stringMaximum("aaaa","aaa","aa");
-        Assert.assertEquals("aaaa", actualValue);
-    }
-    @Test
-    public void maximumString_atPositionTwo_shouldBeTrue() {
-        String  actualValue = FindMaximum.stringMaximum("aaa","aaaa","aa");
-        Assert.assertEquals("aaaa", actualValue);
-    }
-    @Test
-    public void maximumString_atPositionThree_shouldBeTrue() {
-        String  actualValue = FindMaximum.stringMaximum("aa","aaa","aaaa");
-        Assert.assertEquals("aaaa", actualValue);
-    }
+
 }
