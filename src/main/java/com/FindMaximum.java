d6daf15e8c89ee42c5f7b53bf28e4c20b472d958
Seetharamulu T <thumurugoti.seetharamulu@gmail.com>
2020-06-21 20:40:33 +0530

    Added: multiple inputs

--- src/main/java/com/FindMaximum.java
+++ src/main/java/com/FindMaximum.java
@@ -1,27 +1,19 @@
 package com;
 
-public class FindMaximum <E extends Comparable<E>>{
-    private E inputone;
-    private E inputTwo;
-    private E inputThree;
+import java.util.Arrays;
 
-    public FindMaximum(E inputOne,E inputTwo,E inputThree) {
-        this.inputone = inputOne;
-        this.inputTwo = inputTwo;
-        this.inputThree = inputThree;
+public class FindMaximum <E extends Comparable<E>>{
+    private E[] inputArray;
+
+    public FindMaximum(E[] inputArray) {
+        this.inputArray = inputArray ;
     }
     public E maximum(){
-         return FindMaximum.maximumValue(this.inputone,this.inputTwo,this.inputThree);
+         return FindMaximum.maximumValue(this.inputArray);
     }
 
-    public static <E extends Comparable<E>> E maximumValue(E inputOne,E inputTwo,E inputThree){
-        E max = inputOne;
-        if (inputTwo.compareTo(max)>0){
-            max = inputTwo;
-        }
-        if (inputThree.compareTo(max)>0){
-            max = inputThree;
-        }
-        return max;
+    public static <E extends Comparable<E>> E maximumValue(E[] inputArray){
+        Arrays.sort(inputArray);
+        return inputArray[inputArray.length -1];
     }
 }
--- src/test/java/com/MaximumTest.java
+++ src/test/java/com/MaximumTest.java
@@ -6,16 +6,18 @@ import org.junit.Test;
 public class MaximumTest {
     @Test
     public void maximumInteger() {
-            Assert.assertEquals(3,new FindMaximum(1,2,3).maximum());
+        Integer[] integerArray = {3,6,9,3};
+            Assert.assertEquals(9,new FindMaximum(integerArray).maximum());
     }
     @Test
     public void maximumDouble() {
-        float actualValue = (float) new FindMaximum(1.2f,3.2f,4.5f).maximum();
-        Assert.assertEquals(4.5f,actualValue,0.0f);
+       Float[] floatArray = {2.2f,1.3f,2.0f,4.5f};
+        Assert.assertEquals(4.5f,(float) new FindMaximum(floatArray).maximum(),0.0f);
     }
     @Test
     public void maximumString() {
-        Assert.assertEquals("peach",new FindMaximum("apple","peach","banana").maximum());
+        String[] stringArray = {"apple","peach","banana","grape"};
+        Assert.assertEquals("peach",new FindMaximum(stringArray).maximum());
     }
 
 }
